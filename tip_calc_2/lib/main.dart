2c6e67426e47a1b83c71cfb3c300feff7ac9baa4
SHUBHANKARPOLYGON <shubhankarvishal@gmail.com>
2025-04-15 00:00:39 +0530

    debug_banner_removed

--- tip_calc_2/lib/main.dart
+++ tip_calc_2/lib/main.dart
@@ -11,11 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, //to remove the debug banner from the top right corner
+      title: 'U-Tip',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        
       ),
+      home: null,
     );
   }
 }
